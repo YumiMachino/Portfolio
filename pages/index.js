7656d2d5ecd4ca87e8dccf557b9ceb33b7902cb4
YumiMachino <y.y.1515m@gmail.com>
2021-12-02 15:04:47 -0800

    Add Glitch effect

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import GlitchClip from 'react-glitch-effect/core/GlitchClip';
+
 import Layout from '@/components/Layout';
 import Button from '@/components/Button';
 import CyButton from '@/components/CyButton';
@@ -10,11 +10,17 @@ export default function Home() {
   return (
     <Layout>
       <div className={styles.container}>
-        <GlitchClip>
-          <h1 className={styles.name}>
-            YUMI <br /> MACHINO
-          </h1>
-        </GlitchClip>
+        <h1 className={styles.glitch}>
+          <span>
+            Yumi <br /> Machino
+          </span>
+          Yumi <br /> Machino
+          <span>
+            Yumi <br />
+            Machino
+          </span>
+        </h1>
+
         <h5 className={styles.role}>Front End Developer.</h5>
         <CyButton />
       </div>

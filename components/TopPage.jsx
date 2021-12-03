69eb4d767903a7efbcb6dbc861f83994346b734a
YumiMachino <y.y.1515m@gmail.com>
2021-12-02 19:57:58 -0800

    Add function to pause glitch effect on name

--- components/TopPage.jsx
+++ components/TopPage.jsx
@@ -1,29 +1,40 @@
+import { useState } from 'react';
 import CyButton from '@/components/CyButton';
 import Typist from 'react-typist';
 import styles from '@/styles/components/TopPage.module.scss';
 
-const TopPage = () => (
-  <div className={styles.container}>
-    <h1 className={styles.glitch}>
-      <span>
+const TopPage = () => {
+  const [animate, setAnimate] = useState(true);
+
+  pauseGlitch();
+
+  function pauseGlitch() {
+    setTimeout(() => setAnimate(false), 8000);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={`${animate && styles.animate} ${styles.glitch} `}>
+        <span>
+          Yumi <br /> Machino
+        </span>
         Yumi <br /> Machino
-      </span>
-      Yumi <br /> Machino
-      <span>
-        Yumi <br />
-        Machino
-      </span>
-    </h1>
+        <span>
+          Yumi <br />
+          Machino
+        </span>
+      </h1>
 
-    <h5 className={styles.role}>
-      <Typist>
-        <Typist.Delay ms={300} />
-        Front End Developer.
-      </Typist>
-    </h5>
+      <h5 className={styles.role}>
+        <Typist>
+          <Typist.Delay ms={300} />
+          Front End Developer.
+        </Typist>
+      </h5>
 
-    <CyButton />
-  </div>
-);
+      <CyButton />
+    </div>
+  );
+};
 
 export default TopPage;
